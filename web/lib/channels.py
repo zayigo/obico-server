99b16dc79347080e0c5c7a6dc4defaf2e82ad554
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-04-19 20:40:13 -0700

    Revert async consumer: aa991eefdfd43c51719386689493ffbf5b1906bc a19dd32e6e6d64845dd7159eb7ca927154d76e67 d07270f0173dac7ba0b83963d1e98036f02ed82d a3579ae8da6a6e4c51033ddec77acf21a6b7d6c8 c3f3b9d5959f021907401ef2f15b1167f7225083 980ba636b66d2c4d5652207364e115dce3c0a99b d56a63a450eaf88c01ac2b8572527162e7dbd7aa 4fbec4b3c7cf0e9458342b5c67f3793de4492fa7 9e09ba680584fe5e655564406b5ad81b20ba6ffd 21982fb03dc03337fea140a0987f2086a26743b3 cebc8ec386ed69dcb5f28670cafdd5b55fcae5ca 586e7663eb9e6356cd3963f6aca8e938660fe4fd 9d6a38561587a016ceccdd98a4f74750e73b8476

--- web/lib/channels.py
+++ web/lib/channels.py
@@ -1,16 +1,12 @@
-from asgiref.sync import async_to_sync, sync_to_async
+from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from channels.db import database_sync_to_async
 import json
 import time
 from django.dispatch import receiver
 
 from . import cache
 
-CHANNEL_CONSIDERED_ALIVE_IF_TOUCHED_IN_SECS = {
-    '*': 1200,
-    'p_octo': 120,
-}
+CHANNEL_CONSIDERED_ALIVE_IF_TOUCHED_IN_SECS = 1200
 
 
 def octo_group_name(printer_id):
@@ -29,42 +25,29 @@ def octoprinttunnel_group_name(printer_id):
     return 'octoprinttunnel.{}'.format(printer_id)
 
 
-async def async_send_msg_to_printer(printer_id, msg_dict, to_channel=None):
+def send_msg_to_printer(printer_id, msg_dict):
     msg_dict.update({
         'type': 'printer.message',  # mapped to -> printer_message in consumer
     })
     layer = get_channel_layer()
-
-    if to_channel is not None:
-        await layer.send(
-            to_channel,
-            msg_dict,
-        )
-    else:
-        await layer.group_send(
-            octo_group_name(printer_id),
-            msg_dict,
-        )
+    async_to_sync(layer.group_send)(
+        octo_group_name(printer_id),
+        msg_dict,
+    )
 
 
-send_msg_to_printer = async_to_sync(async_send_msg_to_printer)
-
-
-async def async_send_message_to_web(printer_id, msg_dict):
+def send_message_to_web(printer_id, msg_dict):
     msg_dict.update({'type': 'web.message'})    # mapped to -> web_message in consumer
     layer = get_channel_layer()
-    await layer.group_send(
+    async_to_sync(layer.group_send)(
         web_group_name(printer_id),
         msg_dict,
     )
 
 
-send_msg_to_web = async_to_sync(async_send_message_to_web)
-
-
-async def async_send_status_to_web(printer_id):
+def send_status_to_web(printer_id):
     layer = get_channel_layer()
-    await layer.group_send(
+    async_to_sync(layer.group_send)(
         web_group_name(printer_id),
         {
             'type': 'printer.status',         # mapped to -> printer_status in consumer
@@ -72,12 +55,9 @@ async def async_send_status_to_web(printer_id):
     )
 
 
-send_status_to_web = async_to_sync(async_send_status_to_web)
-
-
-async def async_send_janus_to_web(printer_id, msg):
+def send_janus_to_web(printer_id, msg):
     layer = get_channel_layer()
-    await layer.group_send(
+    async_to_sync(layer.group_send)(
         janus_web_group_name(printer_id),
         {
             'type': 'janus.message',         # mapped to -> janus_message in consumer
@@ -85,70 +65,39 @@ async def async_send_janus_to_web(printer_id, msg):
         }
     )
 
-send_janus_to_web = async_to_sync(async_send_janus_to_web)
 
-
-async def async_send_message_to_octoprinttunnel(group_name, data):
+def send_message_to_octoprinttunnel(group_name, data):
     msg_dict = {
         # mapped to -> octoprinttunnel_message in consumer
         'type': 'octoprinttunnel.message',
         'data': data
     }
     layer = get_channel_layer()
-    await layer.group_send(
+    async_to_sync(layer.group_send)(
         group_name,
         msg_dict,
     )
 
 
-send_message_to_octoprinttunnel = async_to_sync(async_send_message_to_octoprinttunnel)
-
-
-async def async_broadcast_ws_connection_change(group_name):
+def broadcast_ws_connection_change(group_name):
     (group, printer_id) = group_name.split('.')
     if group == 'p_web':
-        await async_send_viewing_status(
-            printer_id,
-            await async_get_num_ws_connections(group_name))
+        send_viewing_status(printer_id, get_num_ws_connections(group_name))
     if group == 'p_octo':
-        if await async_get_num_ws_connections(group_name) <= 0:
-            await sync_to_async(cache.printer_status_delete)(printer_id)
-        await async_send_status_to_web(printer_id)
+        if get_num_ws_connections(group_name) <= 0:
+            cache.printer_status_delete(printer_id)
+        send_status_to_web(printer_id)
 
 
-broadcast_ws_connection_change = async_to_sync(async_broadcast_ws_connection_change)
-
-
-async def async_send_viewing_status(printer_id, viewing_count=None, to_channel=None):
+def send_viewing_status(printer_id, viewing_count=None):
     if viewing_count is None:
-        viewing_count = await async_get_num_ws_connections(web_group_name(printer_id))
+        viewing_count = get_num_ws_connections(web_group_name(printer_id))
 
-    await async_send_msg_to_printer(
-        printer_id,
-        {'remote_status': {'viewing': viewing_count > 0}},
-        to_channel=to_channel,
-    )
-
-send_viewing_status = async_to_sync(async_send_viewing_status)
-
-
-async def async_send_should_watch_status(printer, to_channel=None):
-    await async_send_msg_to_printer(
-        printer.id,
-        {'remote_status': {'should_watch': await database_sync_to_async(printer.should_watch)()}},
-        to_channel=to_channel,
-    )
-
-
-send_should_watch_status = async_to_sync(async_send_should_watch_status)
+    send_msg_to_printer(printer_id, {'remote_status': {'viewing': viewing_count > 0}})
 
 
 async def async_get_num_ws_connections(group_name, threshold=None, current_time=None):
-    if threshold is None:
-        threshold = CHANNEL_CONSIDERED_ALIVE_IF_TOUCHED_IN_SECS.get(
-            group_name.split('.')[0],
-            CHANNEL_CONSIDERED_ALIVE_IF_TOUCHED_IN_SECS['*'])
-
+    threshold = threshold if threshold is not None else CHANNEL_CONSIDERED_ALIVE_IF_TOUCHED_IN_SECS
     current_time = time.time() if current_time is None else current_time
     chlayer = get_channel_layer()
     async with chlayer.connection(chlayer.consistent_hash(group_name)) as conn:
@@ -159,10 +108,10 @@ async def async_get_num_ws_connections(group_name, threshold=None, current_time=
 get_num_ws_connections = async_to_sync(async_get_num_ws_connections)
 
 
-async def async_touch_channel(group_name, channel_name):
+async def touch_channel(group_name, channel_name):
     chlayer = get_channel_layer()
     # group_add adds or updates existing channel in a redis sorted set,
     # and sets current time as score.. just what we need
     await chlayer.group_add(group_name, channel_name)
 
-touch_channel = async_to_sync(async_touch_channel)
+touch_channel = async_to_sync(touch_channel)
